dc8420786265d5f6edec69d9a27f902be9d8ef52
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-11-08 21:00:51 +0700

    Add soft-stop script and fix cleanup script.

--- cleanup.sh
+++ cleanup.sh
@@ -8,4 +8,4 @@ docker rmi docker-elk_logstash
 docker rmi docker-elk_filebeat
 docker rmi docker-elk_nginx
 
-rm -f redis/redis.conf logstash/logs.conf
+rm -f redis/redis.conf logstash/logs.conf filebeat/filebeat.yml
