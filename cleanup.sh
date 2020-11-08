7403e4ba27d55528759e29b6a151f9f474fc26ff
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-11-08 22:34:31 +0700

    Fix cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -2,10 +2,9 @@
 
 docker-compose down
 
-docker volume rm docker-elk_data01 docker-elk_data02 docker-elk_data03 docker-elk_redis docker-elk_logstash
+docker volume rm docker-elk_data01 docker-elk_data02 docker-elk_data03 docker-elk_redis docker-elk_logstash docker-elk_grafana
 docker rmi docker-elk_redis
 docker rmi docker-elk_logstash
 docker rmi docker-elk_filebeat
-docker rmi docker-elk_nginx
 
 rm -f redis/redis.conf logstash/logs.conf filebeat/filebeat.yml
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd $(dirname $0)
+mkdir -p ${PWD}/data/nginx/logs/
 scp user@domain:/var/log/nginx/access_json.log ${PWD}/data/nginx/logs/ > /dev/null
