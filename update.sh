ff9fdeb6e928cc9592b616bdd3828a6b6228143e
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-11-10 21:31:22 +0700

    Improve update script

--- update.sh
+++ update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+ELKUSER=$1
+ELKDOMAIN=$2
 cd $(dirname $0)
 mkdir -p ${PWD}/data/nginx/logs/
-scp user@domain:/var/log/nginx/access_json.log ${PWD}/data/nginx/logs/ > /dev/null
+scp ${ELKUSER}@${ELKDOMAIN}:/var/log/nginx/access_json.log ${PWD}/data/nginx/logs/file1 > /dev/null
+scp ${ELKUSER}@${ELKDOMAIN}:/var/log/nginx/access_json.log.1 ${PWD}/data/nginx/logs/file0 > /dev/null
+
+if [ -e ${PWD}/data/nginx/logs/file0 ];
+then
+  cat ${PWD}/data/nginx/logs/file0 ${PWD}/data/nginx/logs/file1 > ${PWD}/data/nginx/logs/access_json.log
+else
+  mv ${PWD}/data/nginx/logs/file1 ${PWD}/data/nginx/logs/access_json.log
+fi
