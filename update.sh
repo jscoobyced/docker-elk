0aa1cd65434f9bd1c71a0a413e68c5f52ee697e9
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-12-20 10:26:22 +0700

    Improve script to get logs

--- update.sh
+++ update.sh
@@ -4,12 +4,20 @@ ELKUSER=$1
 ELKDOMAIN=$2
 cd $(dirname $0)
 mkdir -p ${PWD}/data/nginx/logs/
-scp ${ELKUSER}@${ELKDOMAIN}:/var/log/nginx/access_json.log ${PWD}/data/nginx/logs/file1 > /dev/null
-scp ${ELKUSER}@${ELKDOMAIN}:/var/log/nginx/access_json.log.1 ${PWD}/data/nginx/logs/file0 > /dev/null
 
-if [ -e ${PWD}/data/nginx/logs/file0 ];
-then
-  cat ${PWD}/data/nginx/logs/file0 ${PWD}/data/nginx/logs/file1 > ${PWD}/data/nginx/logs/access_json.log
-else
-  mv ${PWD}/data/nginx/logs/file1 ${PWD}/data/nginx/logs/access_json.log
-fi
+ssh ${ELKUSER}@${ELKDOMAIN} /bin/bash <<'ENDSSH'
+cd /tmp
+rm -Rf nginxlogs
+mkdir nginxlogs
+cd nginxlogs
+cp /var/log/nginx/access_json.log* .
+gunzip access_json.log*.gz
+cat * > access_json_all.log
+gzip access_json_all.log
+rm *.log
+ENDSSH
+
+scp ${ELKUSER}@${ELKDOMAIN}:/tmp/nginxlogs/access_json_all.log.gz ${PWD}/data/nginx/logs/ > /dev/null
+cd ${PWD}/data/nginx/logs/
+gunzip access_json_all.log.gz
+mv access_json_all.log access_json.log
